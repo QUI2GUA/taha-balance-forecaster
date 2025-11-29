97cb3080ae1935961c9ec87f0ff46ed8e1937f1a
QUI2GUA <QUI2GUA@users.noreply.github.com>
2025-11-30 06:11:28 +1000

    Update next.config.js for TypeScript and ESLint settings

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'standalone',
+  
+  // Dangerously allow production builds to successfully complete even if
+  // your project has type errors.
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  
+  // Dangerously allow production builds to successfully complete even if
+  // your project has ESLint errors.
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
+
+module.exports = nextConfig
