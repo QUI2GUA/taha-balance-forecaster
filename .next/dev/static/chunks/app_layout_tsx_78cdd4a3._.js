3ae4f923ea5512585e870b593d3502c5db68e5c2
elvin.quitugua <elvin.quitugua@gmail.com>
2025-11-30 15:41:08 +0000

    Update ENV and package.json

--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++ .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
@@ -2,7 +2,8 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__077fd7b5._.css",
-  "static/chunks/_a1234c90._.js"
+  "static/chunks/node_modules_6cdc1f31._.js",
+  "static/chunks/components_ui_sonner_tsx_f4d78232._.js"
 ],
     source: "dynamic"
 });
