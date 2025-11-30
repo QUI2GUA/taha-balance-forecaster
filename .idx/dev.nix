4609a9b3e5f2a6b71d33d7b83c7250a81fe13072
elvin.quitugua <elvin.quitugua@gmail.com>
2025-11-30 07:33:48 +0000

    Update

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,8 +1,8 @@
 {pkgs}: {
   channel = "stable-24.05";
   packages = [
-    pkgs.nodejs_20,
-    pkgs.docker,
+    pkgs.nodejs_20
+    pkgs.docker
     pkgs.docker-compose
   ];
   idx.extensions = [
